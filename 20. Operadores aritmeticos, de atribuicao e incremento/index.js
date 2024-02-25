4ebab0c52aa3c06bc13aee3137d37c3bdcc9e626
alexia11100 <alexiasacerdotedeoliveira@gmail.com>
2024-02-25 20:50:21 -0300

    Aula 20

---
+++ 20. Operadores aritmeticos, de atribuicao e incremento/index.js
@@ -0,0 +1,67 @@
+// Aritmetico 
+console.log("Aritmético");
+let num1 = 10;
+let num2 = 5;
+console.log("Adição", num1 + num2);
+console.log("Subtração",num1 - num2);
+console.log("Multiplicação",num1*num2);
+console.log("Divisão",num1 / num2);
+console.log("Resto da divisão",num1 % num2);
+console.log("Potenciação",num1 ** num2);
+console.log();
+
+// Incrementação e Decrementação
+console.log("Incrementação e Decrementação");
+
+//Pre incremento
+let contador = 0;
+console.log(++contador);
+
+// Pos incremento
+let contador2 = 0;
+console.log(contador2++);
+
+//Pre decremento
+let contador3 = 0;
+console.log(--contador3);
+
+// Pos decremento
+let contador4 = 0;
+console.log(contador4--);
+console.log();
+
+//Operadores aritmético de atribuição
+console.log("Operadores aritmético de atribuição");
+
+let contador5 = 0;
+contador5 += 2;
+contador5 += 2;
+contador5 += 2;
+console.log(contador5);
+
+let contador6 = 2;
+contador6 *= 2;
+contador6 *= 2;
+contador6 *= 2;
+console.log(contador6);
+console.log();
+
+// Conversão de tipo
+console.log("Conversão de tipo")
+const n1 = 10;
+const n2 = parseInt('5');
+console.log(n1 * n2);
+
+const n3 = 10;
+const n4 = parseFloat('5.2');
+console.log(n3 + n4);
+
+const n5 = 10;
+const n6 = Number('5.2');
+console.log(n5 + n6);
+
+
+
+
+
+
