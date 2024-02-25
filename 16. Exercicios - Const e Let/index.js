d42b6f5869af9a7402d33f65fe4b6c4123272679
alexia11100 <alexiasacerdotedeoliveira@gmail.com>
2024-02-24 21:07:23 -0300

    Aula 16

---
+++ 16. Exercicios - Const e Let/index.js
@@ -0,0 +1,11 @@
+const nome = "Alexia";
+const sobrenome = "Sacerdote de Oliveira";
+const idade = "22";
+const peso = "89";
+const altura = 1.82;
+const imc = peso / (altura * altura);
+const anoDeNascimento = 2001
+
+console.log(nome, sobrenome, "tem", idade, "anos, pesa", peso, "kg");
+console.log( "tem", altura, "de altura e seu imc é", imc);
+console.log(nome, "nasceu em", anoDeNascimento);
